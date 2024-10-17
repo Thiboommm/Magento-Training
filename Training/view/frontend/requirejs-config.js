7ccb4f797a613d3512f2f426a135b04cc02a2911
Thiboommm <thiboom_hu@yahoo.com>
2024-10-18 01:44:02 +0700

    Temporary removed Training module

--- Training/view/frontend/requirejs-config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    config: {
-        mixins: {
-            'Convert_Training/js/externalUrl': {
-                'Convert_Training/js/model/console-log-mixin': true
-            }
-        }
-    }
-}
